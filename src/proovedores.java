d2629dbf6823eed54f13ce82af499ed8c786086f
josafat <josa221001@gmail.com>
2022-09-04 18:29:20 -0500

    add: creando el menu y desplegando datos

--- src/proovedores.java
+++ src/proovedores.java
@@ -1,39 +1,80 @@
 import java.util.Scanner;
+import java.util.*;
 public class proovedores {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int proovedores = 0;
+        int menu = 0;
 
         System.out.println("Ingrese el numero de proovedores: ");
         proovedores = sc.nextInt();
 
-        int[] provArray = new int[proovedores];
+        //int[] provArray = new int[proovedores];
         String[] nameProovedores = new String[proovedores];
         String[] nameCity = new String[proovedores];
         int[] numberProduct = new int[proovedores];
 
-        for (int i = 0; i < provArray.length; i++){
+        for (int i = 0; i < proovedores; i++) {
 
-            System.out.println("Ingrese el nombre del proovedor "+ i);
+            sc.nextLine();
+
+            System.out.println("Ingrese el nombre del proovedor " + i);
             nameProovedores[i] = sc.nextLine();
 
-            System.out.println("Ingrese la ciudad del proovedor "+ i);
+            System.out.println("Ingrese la ciudad del proovedor " + i);
             nameCity[i] = sc.nextLine();
 
-            System.out.println("Ingrese el numero de productos del proovedor "+ i);
+            System.out.println("Ingrese el numero de productos del proovedor " + i);
             numberProduct[i] = sc.nextInt();
         }
 
+        System.out.println("Datos cargados correctamente!");
 
-        for (int i = 0; i < provArray.length; i++){
-            System.out.println(nameProovedores);
-            System.out.println(nameCity);
-            System.out.println(numberProduct);
-        }
+        do {
+            System.out.println("--------------- Gestion de proovedores ---------------");
+            System.out.println("------------------------ Menu ------------------------");
+            System.out.println("1.- Agregar proovedor");
+            System.out.println("2.- Actualizar datos proovedor");
+            System.out.println("3.- Actualizar articulos del proovedor");
+            System.out.println("4.- Mostrar todos los proovedores");
+            System.out.println("5.- Salir");
+
+            int opc = sc.nextInt();
+
+            switch (opc) {
+                case 1:
+                    System.out.println("********** Agregar proovedor ***********");
+                    break;
+
+                case 2:
+                    System.out.println("********** Actualizar datos ***********");
+                    break;
+
+                case 3:
+                    System.out.println("********** Actualizar artículos ***********");
+                    break;
+
+                case 4:
+                    System.out.println("********** Mostrar todos los proovedores ***********");
+                    for (int i = 0; i < proovedores; i++) {
+                        System.out.println("Nombre del proovedor: " + nameProovedores[i]);
+                        System.out.println("Ciudad de residencia: " + nameCity[i]);
+                        System.out.println("Cantidad de productos: " + numberProduct[i] + "\n");
+                    }
+
+                    System.out.println("******************************************************\n\n");
+                    break;
+
+                default:
+                    System.out.println("*********** Operacion no valida **************");
+                    break;
 
 
+            }
 
+
+        }while (menu != 5) ;
     }
 }
